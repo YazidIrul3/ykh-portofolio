e4c3cbf69ca8a4ad20bd80d071df43c3bd0ecb0d
zidcraft879@gmail.com <zidcraft879@gamil.com>
2025-01-06 20:20:31 +0700

    develop:3

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 import HomePage from "@/Components/Layouts/Home";
-import "./globals.css";
 
 export default function Home() {
   return (
     <div>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
