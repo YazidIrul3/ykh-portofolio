9d82e1deaed4dd993fe1af67e75c4c3a66e321da
YazidIrul3 <zidcraft879@gmail.com>
2025-06-19 16:15:00 +0700

    ganti cv dan penambahan sertifikat serta perbaikan tampilan detailCertificaqte

--- src/Components/Fragments/CertificateDetail.jsx
+++ src/Components/Fragments/CertificateDetail.jsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 const CertificateDetail = ({item}) => {
     return(
-        <div className=" flex justify-center items-center bg-slate-900 bg-opacity-30">
-            <Image src={item} className="object-contain p-2 w-fit h-fit bg-slate-50 rounded-lg" alt=""/>
+        <div className=" flex justify-center items-center bg-slate-900 bg-opacity-30 mx-2">
+            <Image src={item} className="object-contain p-2 w-fit min-h-[400px] bg-slate-50 rounded-lg" alt=""/>
         </div>
     )
 }
--- src/helpers/certificate.js
+++ src/helpers/certificate.js
@@ -3,6 +3,10 @@ import images from "./images";
 export const certificate = [
   {
     id: 1,
-    image: images.certificate1,
+    image: images.certificates.nusabot,
+  },
+  {
+    id: 2,
+    image: images.certificates.lks2025,
   },
 ];
